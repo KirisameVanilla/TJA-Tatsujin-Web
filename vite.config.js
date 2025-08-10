a79f01512ac6f1a3fcb979c21308e216238ad97a
KirisameVanilla <118162831+KirisameVanilla@users.noreply.github.com>
2025-08-10 11:39:03 +0800

    chore: project structure

--- vite.config.js
+++ vite.config.js
@@ -12,19 +12,13 @@ function minifyJsonPlugin() {
     name: 'minify-json',
     apply: 'build',
     closeBundle() {
-      // 1. 复制 alias.json 和 style.css 到 dist 目录
+      // 1. 复制 alias.json 到 dist 目录
       const rootAlias = path.resolve(__dirname, 'alias.json');
-      const rootCSS = path.resolve(__dirname, 'style.css');
       const publicDir = path.resolve(__dirname, 'dist');
       const publicAlias = path.join(publicDir, 'alias.json');
-      const publicCSS = path.join(publicDir, 'style.css');
       if (fs.existsSync(rootAlias)) {
         if (!fs.existsSync(publicDir)) fs.mkdirSync(publicDir);
         fs.copyFileSync(rootAlias, publicAlias);
-      }
-      if (fs.existsSync(rootCSS)) {
-        if (!fs.existsSync(publicDir)) fs.mkdirSync(publicDir);
-        fs.copyFileSync(rootCSS, publicCSS);
       }
 
       // 2. 压缩 dist 下所有 json
@@ -42,10 +36,16 @@ function minifyJsonPlugin() {
 }
 
 export default {
+  root: '.',
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    assetsDir: './assets'
+    assetsDir: './assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
   },
   plugins: [minifyJsonPlugin(), tailwindcss()],
   define: {
